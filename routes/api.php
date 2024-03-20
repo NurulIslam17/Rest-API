50057da184ef9a2ef4428fa838e18915277ee47e
@Nurul97 <nurulislamnextgenitltd.com>
2024-03-20 22:29:07 +0600

    Product show to auth user

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,11 @@ use Illuminate\Support\Facades\Route;
 
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserAuthController;
 
 Route::post('register',[UserAuthController::class,'register']);
 Route::post('login',[UserAuthController::class,'login']);
 
 Route::post('logout',[UserAuthController::class,'logout'])->middleware('auth:sanctum');
+Route::apiResource('product', ProductController::class)->middleware('auth:sanctum');
