b9242db174e29cf9622572d9cc834e09d23b3632
@Nurul97 <nurulislamnextgenitltd.com>
2024-03-23 21:39:40 +0600

    Localization

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,12 +13,86 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 </head>
 
-<body style="background: rgb(117, 116, 116)">
-    <div class="position-absolute top-50 start-50 translate-middle">
-        <h1 class="text-center ubuntu-regular text-light">API DEVELOPMENT</h1>
-    </div>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark text-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('img/logo.png') }}" class="rounded-circle" style="height: 55px; width:55px;"
+                    alt="" srcset="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page"
+                            href="#">{{ __('messages.Home') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page"
+                            href="#">{{ __('messages.About') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page"
+                            href="#">{{ __('messages.Blog') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page"
+                            href="#">{{ __('messages.Contact') }}</a>
+                    </li>
+                </ul>
+                <div class="d-flex text-dark">
+
+                    <select class="form-control changeLang">
+                        <option value="en" {{ session()->get('locale') == 'en' ? 'selected' : '' }}>English</option>
+                        <option value="sp" {{ session()->get('locale') == 'sp' ? 'selected' : '' }}>Spanish</option>
+                        <option value="bn" {{ session()->get('locale') == 'bn' ? 'selected' : '' }}>Bangla</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <section class="container mt-5">
+        <div class="row">
+            <div class="col-md-8">
+                <p>
+                    Laravel is a web application framework with expressive, elegant syntax. A web framework provides a
+                    structure and starting point for creating your application, allowing you to focus on creating
+                    something
+                    amazing while we sweat the details. Laravel strives to provide an amazing developer experience while providing powerful features such as
+                    thorough dependency injection, an expressive database abstraction layer, queues and scheduled jobs,
+                    unit
+                    and integration testing, and more.
+                </p>
+                <p>
+                    Whether you are new to PHP web frameworks or have years of experience, Laravel is a framework that
+                    can
+                    grow with you. We'll help you take your first steps as a web developer or give you a boost as you
+                    take
+                    your expertise to the next level. We can't wait to see what you build.
+                </p>
+            </div>
+
+            <div class="col-md-8">
+                fvbfgbfgb
+            </div>
+        </div>
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script type="text/javascript">
+        var url = "{{ route('changeLang') }}";
+
+        $(".changeLang").change(function() {
+            window.location.href = url + "?lang=" + $(this).val();
+        });
     </script>
 </body>
 
