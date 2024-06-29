fb5057a4199a56a3baad443d662c9e9cc6c9597a
@Nurul97 <nurulislamnextgenitltd.com>
2024-06-29 18:23:55 +0600

    Updates

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use PHPUnit\Framework\Constraint\FileExists;
 
 class UserController extends Controller
 {
@@ -21,7 +23,27 @@ class UserController extends Controller
 
     public function update_profile(Request $request){
 
-        
-        dd($request->all());
+        $user = User::where('id',auth()->user()->id)->first();
+        $is_email_exist = User::where('email',$request->email)->exists();
+
+        if ($is_email_exist && $user->email != $request->email) {
+            Log::error("Email Exist Error : This Email is already Used." );
+            return back();
+        }
+
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email
+        ];
+
+
+        if ($request->hasFile('image')) {
+            $imageName = time().'.'.$request->image->extension();
+            $request->image->move(public_path('images'), $imageName);
+            $path = 'images/'.$imageName;
+            $data['profile_photo_path'] = $path;
+        }
+        User::where('id',auth()->user()->id)->update($data);
+        return back();
     }
 }
--- resources/views/backend/include/header.blade.php
+++ resources/views/backend/include/header.blade.php
@@ -3,7 +3,8 @@
         <div class="container-fluid">
             <div class="header-wrap">
                 <form class="form-header" action="" method="POST">
-                    <input class="au-input au-input--xl" type="text" name="search" placeholder="Search for datas &amp; reports..." />
+                    <input class="au-input au-input--xl" type="text" name="search"
+                        placeholder="Search for datas &amp; reports..." />
                     <button class="au-btn--submit" type="submit">
                         <i class="zmdi zmdi-search"></i>
                     </button>
@@ -14,23 +15,23 @@
 
 
                             @isset(auth()->user()->attendence_notification)
-                            <i class="zmdi zmdi-notifications"></i>
-                            <span class="quantity">3</span>
-                            <div class="notifi-dropdown js-dropdown">
-                                <div class="notifi__title">
-                                    <p>You have 1 Notifications</p>
-                                </div>
-                                <div class="notifi__item">
-                                    <div class="bg-c1 img-cir img-40">
-                                        <i class="zmdi zmdi-email-open"></i>
+                                <i class="zmdi zmdi-notifications"></i>
+                                <span class="quantity">3</span>
+                                <div class="notifi-dropdown js-dropdown">
+                                    <div class="notifi__title">
+                                        <p>You have 1 Notifications</p>
                                     </div>
-                                    <div class="content">
-                                        <p>{{auth()->user()->attendence_notification}}</p>
-                                        <span class="date">{{date('d M Y h:i a',strtotime(auth()->user()->created_at))}}</span>
+                                    <div class="notifi__item">
+                                        <div class="bg-c1 img-cir img-40">
+                                            <i class="zmdi zmdi-email-open"></i>
+                                        </div>
+                                        <div class="content">
+                                            <p>{{ auth()->user()->attendence_notification }}</p>
+                                            <span
+                                                class="date">{{ date('d M Y h:i a', strtotime(auth()->user()->created_at)) }}</span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-
                             @endisset
 
                         </div>
@@ -38,23 +39,33 @@
                     <div class="account-wrap">
                         <div class="account-item clearfix js-item-menu">
                             <div class="image">
-                                <img src="{{ asset('asset/images/icon/avatar-01.jpg')}}" alt="John Doe" />
+                                @if (isset(auth()->user()->profile_photo_path))
+                                    <img src="{{ asset(auth()->user()->profile_photo_path) }}" alt="John Doe" />
+                                @else
+                                    <img src="{{ asset('asset/images/icon/avatar-01.jpg') }}" alt="John Doe" />
+                                @endif
                             </div>
                             <div class="content">
-                                <a class="js-acc-btn" href="#">{{auth()->user()->name}}</a>
+                                <a class="js-acc-btn" href="#">{{ auth()->user()->name }}</a>
                             </div>
                             <div class="account-dropdown js-dropdown">
                                 <div class="info clearfix">
                                     <div class="image">
                                         <a href="#">
-                                            <img src="{{ asset('asset/images/icon/avatar-01.jpg')}}" alt="John Doe" />
+                                            @if (isset(auth()->user()->profile_photo_path))
+                                                <img src="{{ asset(auth()->user()->profile_photo_path) }}"
+                                                    alt="John Doe" />
+                                            @else
+                                                <img src="{{ asset('asset/images/icon/avatar-01.jpg') }}"
+                                                    alt="John Doe" />
+                                            @endif
                                         </a>
                                     </div>
                                     <div class="content">
                                         <h5 class="name">
-                                            <a href="#">{{auth()->user()->name}}</a>
+                                            <a href="#">{{ auth()->user()->name }}</a>
                                         </h5>
-                                        <span class="email">{{auth()->user()->email}}</span>
+                                        <span class="email">{{ auth()->user()->email }}</span>
                                     </div>
                                 </div>
                                 <div class="account-dropdown__body">
@@ -75,9 +86,9 @@
                                     <a onclick="document.getElementById('logout').submit();">
                                         <i class="zmdi zmdi-power"></i>Logout</a>
 
-                                        <form action="{{ route('logout')}}" id="logout" method="post">
-                                            @csrf
-                                        </form>
+                                    <form action="{{ route('logout') }}" id="logout" method="post">
+                                        @csrf
+                                    </form>
                                 </div>
                             </div>
                         </div>
