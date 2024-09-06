76eb41f1b0d231b7909b72a78eefc5152bde6afe
@Nurul97 <nurulislamnextgenitltd.com>
2024-09-06 23:34:50 +0600

    Code Refactor

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Backend\CategoryController;
-use App\Http\Controllers\Backend\FlatController;
 use App\Http\Controllers\Backend\PostController;
 use App\Http\Controllers\UserAuthController;
 
@@ -11,6 +10,5 @@ Route::post('register',[UserAuthController::class,'register']);
 Route::post('login',[UserAuthController::class,'login']);
 Route::post('logout',[UserAuthController::class,'logout'])->middleware('auth:sanctum');
 
-Route::resource('flats', FlatController::class);
 Route::resource('categories', CategoryController::class);
 Route::apiResource('posts', PostController::class);
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,8 @@
 <?php
 
-use App\Http\Controllers\Backend\DashboardController;
-use App\Http\Controllers\Backend\FamilyController;
-use App\Http\Controllers\Backend\MultidataInsetConteroller;
-use App\Http\Controllers\Backend\UserController;
-use App\Http\Controllers\GoogleAuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebController;
-use App\Http\Controllers\LocalizatonController;
+
 
 
 Route::get('',[WebController::class,'index'])->name('home');
-// Route::get('lang/change', [LocalizatonController::class, 'changeLang'])->name('changeLang');
-// Route::get('/auth/redirect',[GoogleAuthController::class,'redirect'])->name('google-auth');
-// Route::get('/google-callback',[GoogleAuthController::class,'authCallback']);
-
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified',
-// ])->group(function () {
-
-//     Route::get('/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
-//     Route::get('/multiple-data-handle',[MultidataInsetConteroller::class,'index'])->name('multidata_insert');
-//     Route::get('/family-info',[FamilyController::class,'index'])->name('family_info');
-//     Route::get('/create-family-info',[FamilyController::class,'createFamilyInfo'])->name('create_family');
-
-//     Route::get('/users',[UserController::class,'index'])->name('users');
-//     Route::get('/user-profile',[UserController::class,'user_profile'])->name('user_profile');
-// });
